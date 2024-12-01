a63f10dcbd0952f51fd4b71a0308eb9acc4aeaa1
Cristian Benavides <cdbenavides@unicesar.edu.co>
2024-12-01 14:53:38 -0500

    se modifica

--- fronted/pages/api/usuarios.js
+++ fronted/pages/api/usuarios.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';  // Asegúrate de que tu FastAPI esté corriendo en este puerto
+const API_URL_FASTAPI = 'http://3.228.243.149:30000';  // Asegúrate de que tu FastAPI esté corriendo en este puerto
+const API_URL_NODE = 'http://3.228.243.149:30001';
 
 export async function getUsuarios() {
   try {
-    const response = await axios.get(`${API_URL}/users`);
+    const response = await axios.get(`${API_URL_FASTAPI}/users`);
     return response.data;
   } catch (error) {
     console.error('Error fetching usuarios:', error);
@@ -14,7 +15,7 @@ export async function getUsuarios() {
 
 export async function addUsuario(data) {
   try {
-    const response = await axios.post(`${API_URL}/users`, data);
+    const response = await axios.post(`${API_URL_FASTAPI}/users`, data);
     return response.data;
   } catch (error) {
     console.error('Error adding usuario:', error);
@@ -24,7 +25,7 @@ export async function addUsuario(data) {
 
 export async function updateUsuario(id, data) {
   try {
-    const response = await axios.put(`${API_URL}/users/${id}`, data);
+    const response = await axios.put(`${API_URL_FASTAPI}/users/${id}`, data);
     return response.data;
   } catch (error) {
     console.error('Error updating usuario:', error);
@@ -34,7 +35,7 @@ export async function updateUsuario(id, data) {
 
 export async function deleteUsuario(id) {
   try {
-    const response = await axios.delete(`${API_URL}/users/${id}`);
+    const response = await axios.delete(`${API_URL_FASTAPI}/users/${id}`);
     return response.data;
   } catch (error) {
     console.error('Error deleting usuario:', error);
