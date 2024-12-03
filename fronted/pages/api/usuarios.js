831a043f447182ca1832ccd6b0413488bd52b926
CristianBenavides <cdbenavides@uninorte.edu.co>
2024-12-03 17:59:36 -0500

    esta si es

--- fronted/pages/api/usuarios.js
+++ fronted/pages/api/usuarios.js
@@ -1,41 +1,47 @@
 import axios from 'axios';
-const API_URL_FASTAPI = 'http://3.228.243.149:30000';
-export async function getUsuarios() {
+
+const API_URLS = {
+  fastapi: 'http://3.228.243.149:30000',
+  node: 'http://3.228.243.149:30001/api'
+};
+
+export async function getUsuarios(api = 'fastapi') {
   try {
-    const response = await axios.get(`${API_URL_FASTAPI}/users`);
+    const response = await axios.get(`${API_URLS[api]}/users`);
+
     return response.data;
   } catch (error) {
-    console.error('Error fetching usuarios from FastAPI:', error);
+    console.error(`Error fetching usuarios from ${api} API:`, error);
     return [];
   }
 }
 
-export async function addUsuario(data) {
+export async function addUsuario(api = 'fastapi', data) {
   try {
-    const response = await axios.post(`${API_URL_FASTAPI}/users`, data);
+    const response = await axios.post(`${API_URLS[api]}/users`, data);
     return response.data;
   } catch (error) {
-    console.error('Error adding usuario to FastAPI:', error);
+    console.error(`Error adding usuario to ${api} API:`, error);
     return null;
   }
 }
 
-export async function updateUsuario(id, data) {
+export async function updateUsuario(api = 'fastapi', id, data) {
   try {
-    const response = await axios.put(`${API_URL_FASTAPI}/users/${id}`, data);
+    const response = await axios.put(`${API_URLS[api]}/users/${id}`, data);
     return response.data;
   } catch (error) {
-    console.error('Error updating usuario in FastAPI:', error);
+    console.error(`Error updating usuario in ${api} API:`, error);
     return null;
   }
 }
 
-export async function deleteUsuario(id) {
+export async function deleteUsuario(api = 'fastapi', id) {
   try {
     const response = await axios.delete(`${API_URL_FASTAPI}/users/${id}`);
     return response.data;
   } catch (error) {
-    console.error('Error deleting usuario from FastAPI:', error);
+    console.error(`Error deleting usuario from ${api} API:`, error);
     return null;
   }
 }
